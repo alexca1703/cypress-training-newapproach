d0769000021bcddfa902285bdc0f14b95797061f
alexca1703 <109534257+alexca1703@users.noreply.github.com>
2023-03-29 13:15:51 -0500

    Added CI configuration (#7)
    * Added CI configuration
    
    * Added EoF for yml and .nvmrc files
    
    ---------
    
    Co-authored-by: Alexander Martinez Calle <alexander.martinezc@perficient.com>

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,6 +4,9 @@ export default defineConfig({
   // setupNodeEvents can be defined in either
   // the e2e or component configuration
   e2e: {
+   video: false,
+   screenshotOnRunFailure: false,
+   chromeWebSecurity: false,
    setupNodeEvents(on, config) {
      // modify config values examples
      // config.defaultCommandTimeout = 10000
