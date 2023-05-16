3d0494c8f243a18a422b3f27a452934a3a6ccb83
alexca1703 <109534257+alexca1703@users.noreply.github.com>
2023-05-16 10:42:06 -0500

    Finished step 12 for lists filter and find methods (#12)
    Co-authored-by: Alexander Martinez Calle <alexander.martinezc@perficient.com>

--- cypress/page/inventory.page.ts
+++ cypress/page/inventory.page.ts
@@ -2,16 +2,19 @@ class SauceInventory{
     private shirtSelection: string;
     private inventoryUrl: string;
     private boltTshirtImage: string;
+    private productsSection: string;
+    private productsList: string;
+
 
     constructor() {
-        this.shirtSelection = "Bolt T-Shirt";
+        this.shirtSelection = "a[id='item_1_title_link']";
         this.inventoryUrl = "https://www.saucedemo.com/inventory.html";
         this.boltTshirtImage = "img[alt='Sauce Labs Bolt T-Shirt']";
+        this.productsSection = '.inventory_container';
+        this.productsList = '.inventory_item_name';
     }
 
-    public openProductDetails(): void {
-        cy.contains(this.shirtSelection).click();
-    }
+
 
     public checkInventoryUrl(urlInv:string): void {
         cy.url().should('eq', urlInv);
@@ -19,7 +22,15 @@ class SauceInventory{
 
     public checkProductImageAndText(productName:string): void {
         cy.get(this.boltTshirtImage).invoke('attr', 'alt').should('eq', productName);
-        cy.contains(this.shirtSelection).should('have.text', productName);
+        cy.get(this.shirtSelection).should('have.text', productName);
+    }
+
+    private findProductByName(productName:string): any {
+       return cy.get(this.productsSection).find(this.productsList).filter(`:contains("${productName}")`);
+    }
+
+    public openProductDetails(productNameText:string): void {
+        this.findProductByName(productNameText).click();
     }
 
 
